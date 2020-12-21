c4dd45177b9a5ac9d2fda0f6334611bbf64a226a
lissenko228 <terrariap417@gmail.com>
2020-12-21 16:32:40 +0400

    добавил кнопки для каждого поля, всё еще нужен алерт

--- src/routes/profile.svelte
+++ src/routes/profile.svelte
@@ -4,47 +4,54 @@
 </script>
 
 <style>
-  #f{border-radius:30px 30px 30px 30px;
-  margin-left:650px;
-  background-color:#0bc2db;
-  border-color:black;
-  ;}
-  #but{
-  background-color:#caa777;
-  border-top:none;
-  border-left:none;
-  border-right:1px solid rgba(0,0,0,0.5);
-  border-bottom:1px solid rgba(0,0,0,0.5);
-  outline:none;
-  margin-left:650px;
+#d{margin:auto;
+width:10rem;}
+  #f {
+    border-radius: 30px 30px 30px 30px;
+    background-color: #0bdb38;
+    border-color: black;
+  }
+  #but {
+    background-color: #2abfda;
+    border-top: none;
+    border-left: none;
+    border-right: 1px solid rgba(0, 0, 0, 0.5);
+    border-bottom: 1px solid rgba(0, 0, 0, 0.5);
+    outline: none;
+    
   }
 
-  #but:hover{
-  background-color:#1dd9b3;
+  #but:hover {
+    background-color: #d4e20c;
   }
 
-  #but:active{
-  background-color:#967c66;
-  border-top:1px solid rgba(0,0,0,0.5);
-  border-left:1px solid rgba(0,0,0,0.5);
-  border-bottom:none;
-  border-right:none;
+  #but:active {
+    background-color: #c22257;
+    border-top: 1px solid rgba(0, 0, 0, 0.5);
+    border-left: 1px solid rgba(0, 0, 0, 0.5);
+    border-bottom: none;
+    border-right: none;
   }
 </style>
 
 <TransitionWrapper>
-<input placeholder="Имя" id="f">
-  <br>
-    <input placeholder="Фамилия" id="f">
-    <br>
-      <input placeholder="Адрес" id="f">
-      <br>
-        <input placeholder="Электронная почта" id="f">
-        <br>
-          <input placeholder="Пароль" id="f">
-          <br>
-            <input placeholder="Подтверждение пароля" id="f">
-              <br>
-              <button id="but">Нажми меня</button>
-             </TransitionWrapper>
-            
+  <div id="d">
+  <input placeholder="Имя" id="f" />
+  <button id="but">Нажми меня</button>
+  <br />
+  <input placeholder="Фамилия" id="f" />
+  <button id="but">Нажми меня</button>
+  <br />
+  <input placeholder="Адрес" id="f" />
+  <button id="but">Нажми меня</button>
+  <br />
+  <input placeholder="Электронная почта" id="f" />
+  <button id="but">Нажми меня</button>
+  <br />
+  <input placeholder="Пароль" id="f" />
+  <button id="but">Нажми меня</button>
+  <br />
+  <input placeholder="Подтверждение пароля" id="f" />
+  <button id="but">Нажми меня</button>
+  </div>
+</TransitionWrapper>
